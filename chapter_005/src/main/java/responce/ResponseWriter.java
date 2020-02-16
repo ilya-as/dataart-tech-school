0afed1dd583076dafdb0998cee55a84d70850d56
ilya-as <aslamovi@gmail.com>
2020-02-16 17:04:06 +0400

    buffered writer

--- chapter_005/src/main/java/responce/ResponseWriter.java
+++ chapter_005/src/main/java/responce/ResponseWriter.java
@@ -4,13 +4,15 @@ import java.io.*;
 
 public class ResponseWriter {
 
+    private final int BUFFER_SZE = 1024;
+
     public void write(OutputStream outputStream, Response response) throws IOException {
         InputStream resource = response.getResource();
-        PrintStream answer = new PrintStream(outputStream, true, "UTF-8");
-        answer.print(response.buildHeader());
+        BufferedOutputStream bs = new BufferedOutputStream(outputStream, BUFFER_SZE);
+        bs.write(response.buildHeader().getBytes());
         if (resource != null) {
             int count;
-            byte[] buffer = new byte[1024];
+            byte[] buffer = new byte[BUFFER_SZE];
             while ((count = resource.read(buffer)) != -1) {
                 outputStream.write(buffer, 0, count);
             }
