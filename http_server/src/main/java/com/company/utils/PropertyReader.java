f4f423560a6f03b0a036004e8db8bedd82555838
ilya-as <aslamovi@gmail.com>
2020-03-04 11:20:07 +0400

    refactoring

--- http_server/src/main/java/com/company/utils/PropertyReader.java
+++ http_server/src/main/java/com/company/utils/PropertyReader.java
@@ -4,15 +4,13 @@ import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
-import java.util.Properties;
 
 public class PropertyReader {
-    private String pathToFolder;
+
     private int bufferSize;
-    private int serverPort;
+
     private static final String fileProperties = "app.properties";
-    private final String PROPERTY_PATH_TO_FOLDER = "PATH_TO_FOLDER";
-    private final String PROPERTY_SERVER_PORT = "SERVER_PORT";
+
     private final String PROPERTY_BUFFER_SIZE = "BUFFER_SIZE";
     private final String MESSAGE_ERROR_PORT = "Ошибка чтения значения порта сервера!";
     private final String MESSAGE_ERROR_PROPERTY_FILE = "Ошибка чтения файла настроек %s не обнаружен!";
@@ -22,27 +20,15 @@ public class PropertyReader {
         readProperty();
     }
 
-    public String getPathToFolder() {
-        return pathToFolder;
-    }
-
-    public int getServerPort() {
-        return serverPort;
-    }
-
     public int getBufferSize() {
         return bufferSize;
     }
 
     private void readProperty() {
         File file = new File(getClass().getClassLoader().getResource(fileProperties).getFile());
-        Properties prop = new Properties();
         try {
             FileInputStream fileInputStream = new FileInputStream(file);
             LogManager.getLogManager().readConfiguration(fileInputStream);
-            prop.load(fileInputStream);
-            pathToFolder = LogManager.getLogManager().getProperty(PROPERTY_PATH_TO_FOLDER);
-            serverPort = Integer.parseInt(LogManager.getLogManager().getProperty(PROPERTY_SERVER_PORT));
             bufferSize = Integer.parseInt(LogManager.getLogManager().getProperty(PROPERTY_BUFFER_SIZE));
         } catch (IOException e) {
             LOG.log(Level.WARNING, String.format(MESSAGE_ERROR_PROPERTY_FILE, fileProperties));
